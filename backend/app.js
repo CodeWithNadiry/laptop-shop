856c538d093a7afc67b427f1824d0472c323232a
nadiry <usmannadiry75@gmail.com>
2026-02-02 00:09:25 +0500

    frontend url changed

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use(multer({ storage: fileStorage, fileFilter }).single("image"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
